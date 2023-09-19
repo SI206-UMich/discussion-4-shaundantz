e2aaf02ff762aa0a487b04e3592d7c70ec974f83
Shaun Dantas <sdantas@umich.edu>
2023-09-19 16:19:22 -0400

    fix extra parentheses in init

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -6,7 +6,7 @@ class Rectangle():
     # It sets an instance variable, "height" to the passed argument, height
 
     # YOUR CODE HERE
-    def __init__(self, width, height)):
+    def __init__(self, width, height):
         self.width = width
         self.height = height
 
