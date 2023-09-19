bf3f95a8c17ed01b1a02c7b063174ea118298e46
Shaun Dantas <sdantas@umich.edu>
2023-09-19 16:18:09 -0400

    perimeter

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -54,7 +54,11 @@ class Rectangle():
     # Otherwise, it returns the perimeter of the rectangle.
 
     # YOUR CODE HERE
-    
+    def perimeter(self):
+        if self.verify_input():
+            return 2 * (self.width + self.height)
+        else:
+            return "Invalid input"
 
 
 def main():
